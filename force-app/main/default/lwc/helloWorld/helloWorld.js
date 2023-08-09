cd169c21afe3b3d35c42cab66bb197481abb34f3
karan-extentia <karan.tondare@extentia.com>
2023-08-09 13:26:23 +0530

    Fundamentals - @track decorator

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,10 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     fullName = 'Karan Tondare';
     title = 'Salesforce Developer';
+    @track address = {
+        city: 'Pune',
+        pinCode: 411061,
+        state: 'Maharashtra'
+    }
 
     updateTitle(event) {
         this.title = event.target.value;
+    }
+
+    handleAddressChange(event) {
+        this.address.city = event.target.value;
     }
 }
