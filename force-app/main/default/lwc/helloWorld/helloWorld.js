37d2f4dd63bc773a55c6e49398b48617d94dd101
karan-extentia <karan.tondare@extentia.com>
2023-08-09 13:34:15 +0530

    Fundamentals - getter

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -16,4 +16,16 @@ export default class HelloWorld extends LightningElement {
     handleAddressChange(event) {
         this.address.city = event.target.value;
     }
+
+    users = [ 'karan', 'aalhad'];
+    num1 = 20;
+    num2 = 10;
+
+    get firstUser() {
+        return this.users[0];
+    }
+
+    get multiply() {
+        return this.num1 * this.num2;
+    }
 }
