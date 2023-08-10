aab7829ab308e795e0052ff504b532d0a0ea61e8
karan-extentia <karan.tondare@extentia.com>
2023-08-10 15:22:32 +0530

    Component Communication - Parent->Child Non-Primitive

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParentComponent extends LightningElement {}
+export default class P2cParentComponent extends LightningElement {
+    carouselData = [
+        {
+            src: "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header: "First Card",
+            description: "First card description.",
+            alternativeText: "First card accessible description.",
+            href: "https://www.example.com",
+        },
+        {
+            src: "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header: "Second Card",
+            description: "Second card description.",
+            alternativeText: "Second card accessible description.",
+            href: "https://www.example.com",
+        },
+        {
+            src: "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header: "Thirds Card",
+            description: "Thirds card description.",
+            alternativeText: "Thirds card accessible description.",
+            href: "https://www.example.com",
+        },
+    ]
+}
