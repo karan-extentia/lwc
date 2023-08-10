5b10f4216597a5643cead945050487d5e96a9bf5
karan-extentia <karan.tondare@extentia.com>
2023-08-10 16:52:11 +0530

    Component Communication - Child->Parent using event bubbling

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,10 +3,15 @@ import { LightningElement } from "lwc";
 export default class C2pModalComponent extends LightningElement {
   handleModalClose() {
     const modalCloseEvent = new CustomEvent("close", {
+      bubbles: true,
       detail: {
         message: "modal closed successfully"
       }
     });
     this.dispatchEvent(modalCloseEvent);
+  }
+
+  footerEvent() {
+    console.log("footer event called");
   }
 }
