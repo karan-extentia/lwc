d080e694d73ac7939d8e15c0e3585d4da7a777be
karan-extentia <karan.tondare@extentia.com>
2023-08-14 20:41:38 +0530

    Navigation - navigate to a new record page with default values

--- force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
+++ force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -1,5 +1,6 @@
 import { LightningElement } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
+import { encodeDefaultFieldValues } from "lightning/pageReferenceUtils";
 
 export default class NavigateToObjectPage extends NavigationMixin(
   LightningElement
@@ -10,6 +11,25 @@ export default class NavigateToObjectPage extends NavigationMixin(
       attributes: {
         objectApiName: "Contact",
         actionName: "new"
+      }
+    });
+  }
+
+  navigateToNewRecordWithDefaultValues() {
+    const defaultValue = encodeDefaultFieldValues({
+      FirstName: "Karan",
+      LastName: "Tondare",
+      LeadSource: "Other"
+    });
+
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: "Contact",
+        actionName: "new"
+      },
+      state: {
+        defaultFieldValues: defaultValue
       }
     });
   }
