ca66891b6a78b58289eb8553dffafc55e9af7768
karan-extentia <karan.tondare@extentia.com>
2023-08-16 22:07:53 +0530

    getObjectInfos adapter

--- force-app/main/default/lwc/getObjectInfoDemo/getObjectInfoDemo.js
+++ force-app/main/default/lwc/getObjectInfoDemo/getObjectInfoDemo.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire } from "lwc";
-import { getObjectInfo } from "lightning/uiObjectInfoApi";
+import { getObjectInfo, getObjectInfos } from "lightning/uiObjectInfoApi";
 import ACCOUNT_OBJECT from "@salesforce/schema/Account";
+import OPPORTUNITY_OBJECT from "@salesforce/schema/Opportunity";
 
 export default class GetObjectInfoDemo extends LightningElement {
   //   defaultRecordTypeId;
@@ -21,4 +22,20 @@ export default class GetObjectInfoDemo extends LightningElement {
     objectApiName: ACCOUNT_OBJECT
   })
   objectInfo;
+
+  //
+  objectApiNames = [ACCOUNT_OBJECT, OPPORTUNITY_OBJECT];
+  objectInfos;
+  @wire(getObjectInfos, {
+    objectApiNames: "$objectApiNames"
+  })
+  objectInfosHandler({ data, error }) {
+    if (data) {
+      console.log("objectApiNames data = ", data);
+      this.objectInfos = data;
+    }
+    if (error) {
+      console.log("objectApiNames error = ", error);
+    }
+  }
 }
